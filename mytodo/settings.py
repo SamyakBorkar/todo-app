6007bb9e3f8200b2d8dbfff5729f524daa7945b5
SamyakBorkar <2022bit037@sggs.ac.in>
2025-04-24 22:34:14 +0530

    in settings.py the csrf link is updated.

--- mytodo/settings.py
+++ mytodo/settings.py
@@ -28,7 +28,7 @@ DEBUG = False
 ALLOWED_HOSTS = [".railway.app", '127.0.0.1', 'localhost']
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://blogsite-django-production.up.railway.app'
+    'https://todo-app-production-87c6.up.railway.app'
 ]
 
 
